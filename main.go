67a7d5a31e573a4b34700219dba5249325aec5c5
Karthika Rajagopal <78952344+Karthika-Rajagopal@users.noreply.github.com>
2023-05-06 13:13:00 +0530

    Update main.go

--- main.go
+++ main.go
@@ -17,21 +17,21 @@ func main() {
 	messages := make(chan kafkago.Message, 1000)
 	messageCommitChan := make(chan kafkago.Message, 1000)
 
-	g, ctx := errgroup.WithContext(ctx)
+	g, ctx := errgroup.WithContext(ctx)  //errgroup is created to synchronize the execution of multiple goroutines
 
-	g.Go(func() error {
-		return reader.FetchMessage(ctx, messages)
+	g.Go(func() error {  //fetching message from the kafka
+		return reader.FetchMessage(ctx, messages) //calls the FetchMessage method of the reader 
 	})
 
-	g.Go(func() error {
-		return writer.WriteMessages(ctx, messages, messageCommitChan)
+	g.Go(func() error { //writing message to kafka
+		return writer.WriteMessages(ctx, messages, messageCommitChan) //calls the writemessage method of writer
 	})
 
-	g.Go(func() error {
-		return reader.CommitMessages(ctx, messageCommitChan)
+	g.Go(func() error {  //commiting message to kafka
+		return reader.CommitMessages(ctx, messageCommitChan) //calls the commitmessage method of the reader
 	})
 
-	err := g.Wait()
+	err := g.Wait()  //Any error returned by any of the goroutines will be returned by the Wait method
 	if err != nil {
 		log.Fatalln(err)
 	}
